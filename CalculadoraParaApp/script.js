edef9b9ccb19ab894e1ba683a6f00d9a66946bd8
brendapc <brendacamara@outlook.com>
2020-04-03 20:48:46 -0300

    terminei a calculadora XD

--- CalculadoraParaApp/script.js
+++ CalculadoraParaApp/script.js
@@ -1,3 +1,12 @@
 //2+(km*1.4)+(min*0.26) formula da conta
-var valorcorrida = 40;
-alert('sua corrida deu' + valorcorrida.toFixed);
+
+
+var distanciakm = prompt("distancia");
+var minutosDeCorrida = (distanciakm/0.01)/60;
+var valorcorrida = 2+(distanciakm*1.4)+(minutosDeCorrida*0.26);
+
+function alerte(){
+    document.getElementById('p1').innerHTML ='sua corrida deu ' + valorcorrida.toFixed(2);
+}
+
+//document.write(valorcorrida);
