f212a06545709f86b3a4fdf4f7b252f072d1a2ee
brendapc <brendacamara@outlook.com>
2020-04-04 08:24:30 -0300

    add CSS

--- CalculadoraParaApp/script.js
+++ CalculadoraParaApp/script.js
@@ -1,12 +1,12 @@
 //2+(km*1.4)+(min*0.26) formula da conta
 
 
-var distanciakm = prompt("distancia");
-var minutosDeCorrida = (distanciakm/0.01)/60;
-var valorcorrida = 2+(distanciakm*1.4)+(minutosDeCorrida*0.26);
+
 
 function alerte(){
+    var distanciakm = document.getElementById("myinput").value;
+    var minutosDeCorrida = (distanciakm/0.01)/60;
+    var valorcorrida = 2+(distanciakm*1.4)+(minutosDeCorrida*0.26);
     document.getElementById('p1').innerHTML ='sua corrida deu ' + valorcorrida.toFixed(2);
 }
 
-//document.write(valorcorrida);
