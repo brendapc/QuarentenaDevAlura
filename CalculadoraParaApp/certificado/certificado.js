27a00e0b77dbd221539771ed073eaea8a5448b11
brendapc <brendacamara@outlook.com>
2020-04-10 23:52:48 -0300

    final version

--- CalculadoraParaApp/certificado/certificado.js
+++ CalculadoraParaApp/certificado/certificado.js
@@ -1,52 +1,52 @@
 function gerar() {
-    botaoGerar.disabled = true
+  botaoGerar.disabled = true
+  
+  nome = inputNome.value
+  
+links = [
+    'https://github.com/brendapc/QuarentenaDevAlura/tree/master/CalculadoraParaApp/ValorCorrida',
+    'https://github.com/brendapc/QuarentenaDevAlura/tree/master/CalculadoraParaApp/Bot%C3%A3oDeLikes',
+    'https://github.com/brendapc/QuarentenaDevAlura/blob/master/CalculadoraParaApp/olimpiadas.js',
+    'https://github.com/brendapc/QuarentenaDevAlura/blob/master/CalculadoraParaApp/sorteioapp.html'
     
-    nome = inputNome.value
+  ]
+  
+  titulos = [
+    'Calculadora do Valor',
+    'Botão de Likes',
+    'Quantas olimpiadas houveram',
+    'Promo Sorteio',
     
-    links = [
-      'https://codepen.io/artdiniz/pen/xxwxLvQ',
-      'https://codepen.io/artdiniz/pen/xxwxLvQ',
-      'https://codepen.io/artdiniz/pen/xxwxLvQ',
-      'https://codepen.io/artdiniz/pen/xxwxLvQ',
-      'https://codepen.io/artdiniz/pen/xxwxLvQ'
-    ]
-    
-    titulos = [
-      'Título bonito 1',
-      'Título bonito 2',
-      'Título bonito 3',
-      'Título bonito 4',
-      'Título bonito 5'
-    ]
-   
-    contador = 0
-    while(contador < 1000000) {
-      contador = contador + 1
-      htmlcertificado = emitirCertificado(nome)     
-    }
-    
-    
-    listalinksHTML = ""
-    contadorlinks = 0
-    while(contadorlinks < 5) {
-      listalinksHTML = listalinksHTML + `
-        <li>
-          ${titulos[contadorlinks]}:
-          <a target="_blank" href="${links[contadorlinks]}">
-              ${links[contadorlinks].replace('https://', '')}
-          </a>
-        </li>
-      `
-      
-      contadorlinks = contadorlinks + 1
-    }
-   
-    
-    document.body.innerHTML = htmlcertificado + `
-      <ol>
-        ${listalinksHTML}
-      </ol>
+  ]
+ 
+  contador = 0
+  while(contador < 1000000) {
+    contador = contador + 1
+    htmlcertificado = emitirCertificado(nome)     
+  }
+  
+  
+  listalinksHTML = ""
+  contadorlinks = 0
+  while(contadorlinks < 5) {
+    listalinksHTML = listalinksHTML + `
+      <li>
+        ${titulos[contadorlinks]}:
+        <a target="_blank" href="${links[contadorlinks]}">
+            ${links[contadorlinks].replace('https://', '')}
+        </a>
+      </li>
     `
     
-    print()
+    contadorlinks = contadorlinks + 1
   }
+ 
+  
+  document.body.innerHTML = htmlcertificado + `
+    <ol>
+      ${listalinksHTML}
+    </ol>
+  `
+  
+  print()
+}
